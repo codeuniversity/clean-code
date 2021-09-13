c9aca2cd6a9bf5b19d65b1c947dba60c6d85650b
Alena Valyalkina <avalyalkina@gmail.com>
2021-09-13 18:40:29 +0200

    convert get_file_name into a class method

--- FilePath.py
+++ FilePath.py
@@ -13,9 +13,9 @@ class FilePath:
      def get_file_directory(self, filepath):
              return self.split_path(filepath)[0]
 
+     def get_file_name(self, filepath):
+             return self.split_path(filepath)[1]
 
-def get_file_name(filepath):
-    return FilePath.split_path(filepath)[1]
 
 
 def get_file_extension_type(filepath):
@@ -33,10 +33,10 @@ assert(FilePath().get_file_directory("log/cups/access_log") == "log/cups/")
 assert(FilePath().get_file_directory("log/cups/") == "log/cups/")
 assert(FilePath().get_file_directory("cups/access_log") == "cups/")
 assert(FilePath().get_file_directory("access_log") == "")
-assert(get_file_name("log/cups/access_log") == "access_log")
-assert(get_file_name("log/cups/") == "")
-assert(get_file_name("cups/access_log") == "access_log")
-assert(get_file_name("access_log") == "access_log")
+assert(FilePath().get_file_name("log/cups/access_log") == "access_log")
+assert(FilePath().get_file_name("log/cups/") == "")
+assert(FilePath().get_file_name("cups/access_log") == "access_log")
+assert(FilePath().get_file_name("access_log") == "access_log")
 assert(get_file_extension_type("log/cups/access_log") == "")
 assert(get_file_extension_type("base/FileHelper.cpp") == "cpp")
 assert(get_file_extension_type("base/FileHelper.cpp.bak") == "bak")
