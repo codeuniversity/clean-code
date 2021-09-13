ddcd9fd38183b283849afd2f33d545298bda8bd9
Alena Valyalkina <avalyalkina@gmail.com>
2021-09-13 18:24:27 +0200

    refactor to class, reuse class method

--- FilePath.py
+++ FilePath.py
@@ -1,23 +1,26 @@
-def __split_path(filepath):
-     try:
-         last_slash_index = filepath.rindex('/') + 1
-         file_directory = filepath[:last_slash_index]
-         file_name = filepath[last_slash_index:]
-         return file_directory, file_name
-     except:
-         return '', filepath
+class FilePath:
+
+     @staticmethod
+     def split_path(filepath):
+         try:
+             last_slash_index = filepath.rindex('/') + 1
+             file_directory = filepath[:last_slash_index]
+             file_name = filepath[last_slash_index:]
+             return file_directory, file_name
+         except:
+             return '', filepath
 
 
 def get_file_directory(filepath):
-    return __split_path(filepath)[0]
+    return FilePath.split_path(filepath)[0]
 
 
 def get_file_name(filepath):
-    return __split_path(filepath)[1]
+    return FilePath.split_path(filepath)[1]
 
 
 def get_file_extension_type(filepath):
-    file_name = __split_path(filepath)[1]
+    file_name = FilePath.split_path(filepath)[1]
     try:
         last_dot_index = file_name.rindex('.')
         return file_name[last_dot_index + 1:]
@@ -25,8 +28,8 @@ def get_file_extension_type(filepath):
         return ''
 
 
-assert(__split_path("log/cups/access_log")[0] == "log/cups/")
-assert(__split_path("log/cups/access_log")[1] == "access_log")
+assert(FilePath.split_path("log/cups/access_log")[0] == "log/cups/")
+assert(FilePath.split_path("log/cups/access_log")[1] == "access_log")
 assert(get_file_directory("log/cups/access_log") == "log/cups/")
 assert(get_file_directory("log/cups/") == "log/cups/")
 assert(get_file_directory("cups/access_log") == "cups/")
