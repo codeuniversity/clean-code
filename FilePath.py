48a29327a033b1f7efd5049768bd472a68addb0e
Alena Valyalkina <avalyalkina@gmail.com>
2021-09-13 18:38:20 +0200

    convert get_file_directory into a class method

--- FilePath.py
+++ FilePath.py
@@ -10,9 +10,8 @@ class FilePath:
          except:
              return '', filepath
 
-
-def get_file_directory(filepath):
-    return FilePath.split_path(filepath)[0]
+     def get_file_directory(self, filepath):
+             return self.split_path(filepath)[0]
 
 
 def get_file_name(filepath):
@@ -30,10 +29,10 @@ def get_file_extension_type(filepath):
 
 assert(FilePath.split_path("log/cups/access_log")[0] == "log/cups/")
 assert(FilePath.split_path("log/cups/access_log")[1] == "access_log")
-assert(get_file_directory("log/cups/access_log") == "log/cups/")
-assert(get_file_directory("log/cups/") == "log/cups/")
-assert(get_file_directory("cups/access_log") == "cups/")
-assert(get_file_directory("access_log") == "")
+assert(FilePath().get_file_directory("log/cups/access_log") == "log/cups/")
+assert(FilePath().get_file_directory("log/cups/") == "log/cups/")
+assert(FilePath().get_file_directory("cups/access_log") == "cups/")
+assert(FilePath().get_file_directory("access_log") == "")
 assert(get_file_name("log/cups/access_log") == "access_log")
 assert(get_file_name("log/cups/") == "")
 assert(get_file_name("cups/access_log") == "access_log")
