00b8554eddd11c191c99a33034bda43b3e60a97e
Alena Valyalkina <avalyalkina@gmail.com>
2021-09-14 10:13:18 +0200

    move get_file_extension_type function to FilePath class

--- FilePath.py
+++ FilePath.py
@@ -1,34 +1,33 @@
 class FilePath:
+   file_directory: str
+   file_name: str
 
-     @staticmethod
-     def split_path(filepath):
-         try:
-             last_slash_index = filepath.rindex('/') + 1
-             file_directory = filepath[:last_slash_index]
-             file_name = filepath[last_slash_index:]
-             return file_directory, file_name
-         except:
-             return '', filepath
+    def __init__(self, filepath):
+        try:
+            last_slash_index = filepath.rindex('/') + 1
+            self.file_directory = filepath[:last_slash_index]
+            self.file_name = filepath[last_slash_index:]
+        except:
+            self.file_directory = ''
+            self.file_name = filepath
 
-     def get_file_directory(self, filepath):
-             return self.split_path(filepath)[0]
+    def get_file_directory(self, filepath):
+        return self.split_path(filepath)[0]
 
-     def get_file_name(self, filepath):
-             return self.split_path(filepath)[1]
+    def get_file_name(self, filepath):
+        return self.split_path(filepath)[1]
 
-
-
-def get_file_extension_type(filepath):
-    file_name = FilePath.split_path(filepath)[1]
-    try:
-        last_dot_index = file_name.rindex('.')
-        return file_name[last_dot_index + 1:]
-    except:
-        return ''
+    def get_file_extension_type(self, filepath):
+        file_name = self.split_path(filepath)[1]
+        try:
+            last_dot_index = file_name.rindex('.')
+            return file_name[last_dot_index + 1:]
+        except:
+            return ''
 
 
-assert(FilePath.split_path("log/cups/access_log")[0] == "log/cups/")
-assert(FilePath.split_path("log/cups/access_log")[1] == "access_log")
+assert(FilePath("log/cups/access_log").file_directory == "log/cups/")
+assert(FilePath("log/cups/access_log").file_name == "access_log")
 assert(FilePath().get_file_directory("log/cups/access_log") == "log/cups/")
 assert(FilePath().get_file_directory("log/cups/") == "log/cups/")
 assert(FilePath().get_file_directory("cups/access_log") == "cups/")
@@ -37,6 +36,6 @@ assert(FilePath().get_file_name("log/cups/access_log") == "access_log")
 assert(FilePath().get_file_name("log/cups/") == "")
 assert(FilePath().get_file_name("cups/access_log") == "access_log")
 assert(FilePath().get_file_name("access_log") == "access_log")
-assert(get_file_extension_type("log/cups/access_log") == "")
-assert(get_file_extension_type("base/FileHelper.cpp") == "cpp")
-assert(get_file_extension_type("base/FileHelper.cpp.bak") == "bak")
+assert(FilePath().get_file_extension_type("log/cups/access_log") == "")
+assert(FilePath().get_file_extension_type("base/FileHelper.cpp") == "cpp")
+assert(FilePath().get_file_extension_type("base/FileHelper.cpp.bak") == "bak")
